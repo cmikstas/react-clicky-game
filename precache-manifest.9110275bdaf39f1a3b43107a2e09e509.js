6a0dfc611c8d047c38107cc94babb310de9ee923
Chris Mikstas <chrismikstas@gmail.com>
2020-01-08 22:02:05 -0700

    Updates

---
+++ precache-manifest.9110275bdaf39f1a3b43107a2e09e509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c75f75b6d664003ab73a508986e7f8",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "0f480b0efe721af4041e",
+    "url": "/react-clicky-game/static/css/main.687fa319.chunk.css"
+  },
+  {
+    "revision": "10aa68f0041407f8b1e2",
+    "url": "/react-clicky-game/static/js/2.da758598.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky-game/static/js/2.da758598.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f480b0efe721af4041e",
+    "url": "/react-clicky-game/static/js/main.3e9fa982.chunk.js"
+  },
+  {
+    "revision": "77ed3ad1814e26c4f3ed",
+    "url": "/react-clicky-game/static/js/runtime-main.603ce375.js"
+  }
+]);
